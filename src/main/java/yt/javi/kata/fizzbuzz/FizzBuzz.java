21fe760977ab5ea3332d990b205d7a23987f25a5
javiermadueno <javiermadueno@gmail.com>
2017-03-22 23:48:32 +0100

    Secuence created

--- src/main/java/yt/javi/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/yt/javi/kata/fizzbuzz/FizzBuzz.java
@@ -1,11 +1,28 @@
 package yt.javi.kata.fizzbuzz;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static java.util.Collections.emptyList;
 
 public class FizzBuzz {
-  public List<Object> getFizzBuzzSequence() {
-    return emptyList();
-  }
+
+    public  List<Object> getFizzBuzzSequence() {
+        List<Object> buzzSequence = new ArrayList<>();
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                if (i % 3 != 0) {
+                    buzzSequence.add("Buzz");
+                } else if (i % 5 != 0) {
+                    buzzSequence.add("Fizz");
+                } else {
+                    buzzSequence.add("FizzBuzz");
+                }
+            } else {
+                buzzSequence.add(i);
+            }
+        }
+
+        return buzzSequence;
+    }
 }
